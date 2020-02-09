b84b2dd15dfcbf18baa197d7c5883c5570e8a41e
Yogesh <chavanyg@gmail.com>
2020-02-09 20:04:03 +0530

    config file updated

--- myblog/src/test/java/testCases/BaseClass.java
+++ myblog/src/test/java/testCases/BaseClass.java
@@ -1,7 +1,5 @@
 package testCases;
 
-import io.github.bonigarcia.wdm.ChromeDriverManager;
-import io.github.bonigarcia.wdm.DriverManagerType;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
